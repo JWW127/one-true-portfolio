fad652f24f7c639d15573ab65ff57bafb87e9f72
Joey <19317273+JWW127@users.noreply.github.com>
2022-05-20 20:32:31 -0500

    added more stars

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,20 +1,26 @@
 export const Stars = () => {
-    return (
-        <>
-              <svg className="stars" height="100" width="100">
+  return (
+    <>
+      <svg className="stars" height="100" width="100">
         <image href="./../star1.svg" height="50px" width="50px" />
       </svg>
-      <svg className="stars-two" height="100" width="100">
+      <svg className="stars-two" height="80" width="80">
         <image href="./../star1.svg" height="50px" width="50px" />
       </svg>
       <svg className="stars-three" height="100" width="100">
         <image href="./../star1.svg" height="25px" width="25px" />
       </svg>
-      <svg className="stars-four" height="140" width="140">
-        <image className="star-color" href="./../star1.svg" height="140px" width="140px" />
+      <svg className="stars-five" height="75" width="75">
+        <image href="./../star1.svg" height="20px" width="20px" />
       </svg>
-        </>
-    )
-}
-
-
+      <svg className="stars-four" height="140" width="140">
+        <image
+          className="star-color"
+          href="./../star1.svg"
+          height="140px"
+          width="140px"
+        />
+      </svg>
+    </>
+  );
+};

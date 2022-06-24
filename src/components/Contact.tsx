1b29b449f66cfe52729afdd0e93c2cd3e98a632c
🍟Joey🍟 <19317273+JWW127@users.noreply.github.com>
2022-06-24 00:20:44 -0500

    finished contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,42 @@
+import { BsLinkedin, BsTwitter, BsMailbox } from "react-icons/bs";
 const Contact = () => {
   return (
-    <>
-      <p>Contact</p>
-    </>
+    <div className="contact-container">
+      <h1>Best Ways to Contact Me</h1>
+      <ul>
+        <li>
+          {" "}
+          <a
+            href="https://www.linkedin.com/in/jww127/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p>Linkedin</p>
+            <BsLinkedin />
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a
+            href="https://www.twitter.com/joseph27dev/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p>Twitter</p>
+            <BsTwitter />
+          </a>
+        </li>
+        <li>
+          {" "}
+          <div className="contact-item">
+            <p>
+              <span id="contact-email">Joseph27Dev@gmail.com</span>
+            </p>
+            <BsMailbox />
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 };
 
